3cac0754def627e816b9e1c5aefad892f900cf5e
ChangHyun2 <56799176+ChangHyun2@users.noreply.github.com>
2020-07-26 17:10:06 +0900

    Update observerPattern.js

--- observerPattern.js
+++ observerPattern.js
@@ -15,7 +15,7 @@ ObserverList.prototype.add = function (obj) {
   this.observerList.push(obj);
 };
 ObserverList.prototype.count = function () {
-  this.observerList.length;
+  return this.observerList.length;
 };
 ObserverList.prototype.get = function (index) {
   if (index > -1 && index < this.observerList.length)
